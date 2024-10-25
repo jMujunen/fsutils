7be3f154d373bdf3cbb48682e8a430e85af6c8cf
jMujunen <jMujunen@github.com>
2024-10-24 19:36:30 -0700

    Significantly improve serialization performance by using C data types
    - Converted python data-types to C
    - Compile resulting C code

--- fsutils/DirNode.pyx
+++ fsutils/DirNode.pyx
@@ -1,5 +1,6 @@
+# from fsutils.GenericFile import File
 """Represents a directory. Contains methods to list objects inside this directory."""
-
+import cython
 import datetime
 import os
 import pickle
@@ -10,19 +11,24 @@ from collections.abc import Generator, Iterator
 from pathlib import Path
 from typing import LiteralString
 
-from ProgressBar import ProgressBar
-from size import Size
 from ThreadPoolHelper import Pool
 
-from fsutils.GenericFile import File
-from fsutils.GitObject import Git
-from fsutils.ImageFile import Img
-from fsutils.LogFile import Log
-from fsutils.mimecfg import FILE_TYPES
-from fsutils.VideoFile import Video
+from . import GenericFile
+from . import GitObject
+from . import ImageFile
+from . import LogFile
+from . import mimecfg
+from . import VideoFile
+from . import GitObject
+from . import tools
+from . import GenericFile
+# from . import cimport GenericFile as File
+# from .GenericFile cimport File
+# from fsutils.GenericFile import File
 
 
-class Dir(File):
+
+class Dir(GenericFile.File):
     """A class representing information about a directory.
 
     Attributes
@@ -33,10 +39,10 @@ class Dir(File):
     -------
         - `file_info (other)` :     # Check for `other` in self and return it as an object of `other`
         - `getinfo()` :             # Returns a list of extentions and their count
-        - `__eq__ (other)` :        # Compare properties of two Dir objects
+        - `__eq__ (other)` :        # Compare properties of two "Dir" objects
         - `__contains__ (other)` :  # Check if `other` is present in self
         - `__len__`:                # Return the number of objects in self
-        - `__iter__`  :             # Iterator which yields the appropriate File instance
+        - `__iter__`  :             # Iterator which yields the appropriate GenericFile.File instance
 
 
     Properties
@@ -47,11 +53,11 @@ class Dir(File):
 
     """
 
-    _objects: list[File]
-    encoding: None = None
+    _content: list[GenericFile.File]
+    _encoding: str
 
     def __init__(self, path: str | Path, *args, **kwargs) -> None:
-        """Initialize a new instance of the Dir class.
+        """Initialize a new instance of the "Dir" class.
 
         Parameters
         ----------
@@ -83,12 +89,17 @@ class Dir(File):
         # return [f.name for f in self.objects if not f.is_dir()]
 
     @property
-    def file_objects(self) -> list[File | Log | Img | Video | Git]:
+    def file_objects(
+        self,
+    ) -> list[GenericFile.File | LogFile.Log | ImageFile.Img | VideoFile.Video | GitObject.Git]:
         """Return a list of objects contained in the directory.
+
+        This property iterates over all items in the directory and filters out those that are instances
+        of GenericFile.File,  LogFile.Log, ImageFile.Img, VideoFile.Video, or Git, excluding directories.
 
         Returns
         -------
-            List[File,  Log, Img, Video, Git]: A list of file objects.
+            List[GenericFile.File,  LogFile.Log, ImageFile.Img, VideoFile.Video, Git]: A list of file objects.
 
         """
         return list(filter(lambda x: not isinstance(x, Dir), self))
@@ -102,12 +113,7 @@ class Dir(File):
             return []
 
     @property
-    def rel_directories(self) -> list[str]:
-        """Return a list of subdirectory paths relative to the directory represented by this object."""
-        return [f".{str(folder.path).replace(str(self.parent), "")}" for folder in self.dirs]
-
-    @property
-    def objects(self) -> list[File]:
+    def objects(self) -> list[GenericFile.File]:
         """Return a list of fsutils objects inside self."""
         try:
             self._objects = list(self.__iter__())
@@ -121,27 +127,27 @@ class Dir(File):
         return len(self.files) == 0
 
     @property
-    def images(self) -> list[Img]:
+    def images(self) -> list[ImageFile.Img]:
         """A list of ImageObject instances found in the directory."""
-        return list(filter(lambda x: isinstance(x, Img), self.__iter__()))  # type: ignore
+        return list(filter(lambda x: isinstance(x, ImageFile.Img), self.__iter__()))  # type: ignore
 
     @property
-    def videos(self) -> list[Video]:
+    def videos(self) -> list[VideoFile.Video]:
         """A list of VideoObject instances found in the directory."""
-        return list(filter(lambda x: isinstance(x, Video), self.__iter__()))  # type: ignore
+        return list(filter(lambda x: isinstance(x, VideoFile.Video), self.__iter__()))  # type: ignore
 
     @property
-    def logs(self) -> list[Log]:
-        """A list of Log instances found in the directory."""
-        return list(filter(lambda x: isinstance(x, Log), self.__iter__()))  # type: ignore
+    def logs(self) -> list[LogFile.Log]:
+        """A list of LogFile.Log instances found in the directory."""
+        return list(filter(lambda x: isinstance(x, LogFile.Log), self.__iter__()))  # type: ignore
 
     @property
-    def dirs(self) -> list[File]:
+    def dirs(self) -> list[GenericFile.File]:
         """A list of DirectoryObject instances found in the directory."""
-        return list(filter(lambda x: isinstance(x, Dir), self.__iter__()))
+        return list(filter(lambda x: isinstance(x, "Dir"), self.__iter__()))
 
     @property
-    def describe(self) -> None:
+    def summary(self) -> None:
         """Print a formatted table of each file extention and their count."""
         if not self.metadata:
             self.metadata = defaultdict(int)
@@ -158,34 +164,33 @@ class Dir(File):
             print(f"{key: <{max_key_length}} {value}")
 
     @property
-    def size(self) -> Size:
+    def size(self) -> int:
         """Return the total size of all files and directories in the current directory."""
         if hasattr(self, "_size") and self._size is not None:
             return self._size
-        self._size = Size(
-            sum(
-                Pool().execute(
+        pool = Pool()
+        self._size = sum(
+                pool.execute(
                     lambda x: x.size,
                     self.file_objects,
                     progress_bar=False,
                 ),
-            ),
-        )
+            )
         return self._size
 
     @property
     def size_human(self) -> str:
-        return str(self.size)
+        return tools.format_bytes(self.size)
 
-    def search(self, pattern: str, attr: str = "name") -> list[File]:
+    def search(self, pattern: str, attr: str = "name") -> list[GenericFile.File]:
         """Query the object for files with the given `name | regex` pattern.
 
         Paramaters:
         -----------
             - pattern (str): The regular expression to search for.
-            - attr (str): The attribute of the `File` object to search on.
+            - attr (str): The attribute of the `GenericFile.File` object to search on.
 
-        Return an list of `File` instances if found
+        Return an list of `GenericFile.File` instances if found
         """
         return [obj for obj in self.file_objects if re.search(pattern, getattr(obj, attr))]
 
@@ -237,33 +242,32 @@ class Dir(File):
 
     def __format__(self, format_spec: str, /) -> LiteralString | str:
         pool = Pool()
-        match format_spec:
-            case "videos":
-                print(Video.fmtheader())
-                return "\n".join(
-                    result for result in pool.execute(format, self.videos, progress_bar=False)
-                )
-            case "images":
-                print(Img.fmtheader())
-                return "\n".join(
-                    result for result in pool.execute(format, self.images, progress_bar=False)
-                )
-            case _:
-                return f"Spec {format_spec} is not supported yet"
+        if format_spec == "videos":
+            print(VideoFile.Video.fmtheader())
+            return "\n".join(
+                result for result in pool.execute(format, self.videos, progress_bar=False)
+            )
+        elif format_spec == "images":
+            print(ImageFile.Img.fmtheader())
+            return "\n".join(
+                result for result in pool.execute(format, self.images, progress_bar=False)
+            )
+        else:
+            raise ValueError("Invalid format specifier")
 
-    def __contains__(self, item: File) -> bool:
-        """Is `File` in self?"""  # noqa
+    def __contains__(self, item: GenericFile.File) -> bool:
+        """Is `GenericFile.File` in self?"""  # noqa
         return item.sha256() in self.serialize()
 
     def __hash__(self) -> int:
-        return hash((tuple(self.content), self.is_empty, tuple(self.rel_directories)))
+        return hash((tuple(self.content), self.is_empty))
 
     def __len__(self) -> int:
         """Return the number of items in the object."""
         return len(self.objects)
 
-    def __iter__(self) -> Iterator[File]:
-        """Yield a sequence of File instances for each item in self."""
+    def __iter__(self) -> Iterator[GenericFile.File]:
+        """Yield a sequence of GenericFile.File instances for each item in self."""
         if len(self._objects) == 0:
             for root, _, files in os.walk(self.path):
                 # Yield directories first to avoid unnecessary checks inside the loop
@@ -277,8 +281,8 @@ class Dir(File):
                         self._objects.append(_obj)
         yield from self._objects
 
-    def __eq__(self, other: "Dir", /) -> bool:
-        """Compare the contents of two Dir objects."""
+    def __eq__(self, other: object, /) -> bool:
+        """Compare the contents of two "Dir" objects."""
         return all(
             (
                 isinstance(other, self.__class__),
@@ -292,14 +296,14 @@ class Dir(File):
         )
 
 
-def obj(path: str) -> File | None:
-    """Return a File object for the given path."""
+def obj(path: str) -> GenericFile.File | None:
+    """Return a GenericFile.File object for the given path."""
     pathobj = Path(path)
     if pathobj.is_dir():
         return Dir(path)
     ext = pathobj.suffix.lower()
 
-    for file_type, extensions in FILE_TYPES.items():
+    for file_type, extensions in mimecfg.FILE_TYPES.items():
         if ext in extensions:
             # Dynamically create the class name and instantiate it
             class_name = file_type.capitalize()
@@ -310,9 +314,11 @@ def obj(path: str) -> File | None:
             except FileNotFoundError as e:
                 print(f"{e!r}")
             except AttributeError:
-                return File(path)
+                return GenericFile.File(path)
     try:
-        FileClass = File(path)
+        FileClass = GenericFile.File(path)
     except FileNotFoundError as e:
         return None
-    return File(path)
+    return GenericFile.File(path)
+
+
