b40783c522dd32743d17c978e9940b302601b71c
jMujunen <jMujunen@github.com>
2025-03-29 16:14:50 -0700

    Refactor `setup.py` for better flexibility on compiler flags for Cython compilation

--- fsutils/setup.py
+++ fsutils/setup.py
@@ -1,4 +1,19 @@
 from Cython.Build import cythonize
 from setuptools import setup
+from setuptools.extension import Extension
 
-setup(ext_modules=cythonize(["**/*.pyx"]))
+ext_modules = [
+    Extension(
+        "*",
+        ["**/*.pyx"],
+        libraries=["m"],
+        extra_compile_args=["-ffast-math", "-O3", "-Wall", "-Wextra"],
+    )
+]
+
+setup(
+    name="fsutils",
+    ext_modules=cythonize(ext_modules),
+)
+
+# setup(ext_modules=cythonize(["**/*.pyx"]))
